662aba48cd65d3139298d2ddca8cbbdad5a33a37
Octavio Arruda <oneiros241@gmail.com>
2019-03-01 15:18:22 -0300

    Adjusting some views

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,30 +5,51 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Web.Mvc;
+using System.Collections.Generic;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie() {Name = "Yüü Yüü Hakusho!" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Hitsugaya" },
-                new Customer { Name = "Renji" },
-                new Customer { Name = "Minamino" },
-                new Customer { Name = "Danna" }
-            };
+            var movies = GetMovies();
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customer = customers
-            };
-
-            return View(viewModel);
+            return View(movies);
         }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Sun Tzu war art"},
+                new Movie {Id = 2, Name = "Enter the Dragon"}
+            };
+        }
+
+        // GET: Movies/Random
+            public ActionResult Random()
+            {
+                var movie = new Movie()
+                {
+                    Name = "Last Soldier Alive"
+                };
+
+                var customers = new List<Customer>
+                {
+                    new Customer {Name = "Hiei Jaganshi"},
+                    new Customer {Name = "Kurama Youko"}
+                };
+
+                var viewModel = new RandomMovieViewModel
+                {
+                    Movie = movie,
+                    Customer = customers
+                };
+
+                return View(viewModel);
+            }
+        
     }
 }
